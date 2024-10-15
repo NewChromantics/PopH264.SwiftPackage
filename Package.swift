b4e685e54af95b8152d043155a6ad54e87c89d72
Graham Reeves <graham@grahamreeves.com>
2024-10-15 11:19:44 +0100

    Restored external binary framework

--- Package.swift
+++ Package.swift
@@ -33,8 +33,11 @@ let package = Package(
 		
 		.binaryTarget(
 					name: "PopH264Framework",
-					path: "PopH264.xcframework"
-				),
+					url: "https://github.com/NewChromantics/PopH264/releases/download/v1.3.41/PopH264.xcframework.zip",
+					checksum: "8a378470a2ab720f2ee6ecf4e7a5e202a3674660c31e43d95d672fe76d61d68c"
+				)
+	]
+
 		
 		.target(
 			name: "PopH264Objc",
